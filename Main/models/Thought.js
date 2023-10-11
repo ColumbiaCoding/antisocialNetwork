079c6bf5586916ab09c87e7fb8ce066878bb6e49
ColumbiaCoding <gabriel.regus@gmail.com>
2023-10-11 00:12:44 -0400

    adding working app files

---
+++ Main/models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+// Schema to create a thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+thoughtSchema.virtual('reactionCount').get(function (){
+  return this.reactions.length
+})
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
